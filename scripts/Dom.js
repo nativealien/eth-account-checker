70a17cea1601d726286e556c1d9777de3a7d6175
nativealien <oskar@assarsson.nu>
2024-02-23 02:29:37 +0100

    styling and working transaction changes

--- scripts/Dom.js
+++ scripts/Dom.js
@@ -7,9 +7,11 @@ export default class Dom{
     #accountDetails = document.querySelector('.account-info')
     #transaction = document.querySelector('.receive-bar')
     #content = document.querySelector('.content-info')
+    #msg = document.querySelector('#msg')
 
     #accKey;
     #resKey;
+    #trxHash;
 
     constructor(){
         this.explorer = new Explorer()
@@ -40,57 +42,75 @@ export default class Dom{
         const inputKey = parent.querySelector(`.input-key`)
         const target = parent.querySelector(`.div-mm`)
         const keys = await this.wallet.getAccounts()
+        if(typeof keys === 'object'){
+            this.removeChildren(target, '.input-key')
 
-        this.removeChildren(target, '.input-key')
-
-        keys.forEach( key => {
-            const input = inputKey.cloneNode(true)
-            input.type = 'submit'
-            input.value = key
-            input.style.cursor = 'pointer'
-            input.addEventListener('click', async () => {
-                inputKey.value = key
+            keys.forEach( key => {
+                const input = inputKey.cloneNode(true)
+                input.type = 'submit'
+                input.value = key
+                input.style.cursor = 'pointer'
+                input.addEventListener('click', async () => {
+                    inputKey.value = key
+                })
+                target.appendChild(input)
             })
-            target.appendChild(input)
-        })
+        }else{
+            console.log('Something is wrong..')
+            this.#msg.textContent = 'You dont seem too have Metamask... '
+        }
     }
 
     async eventGo(e){
         if(e.target.parentNode.className === 'account-bar'){
             this.#accKey = e.target.parentNode.querySelector('.input-key').value
             const balance = await this.wallet.checkBalance(this.#accKey)
-            if(this.#accKey !== '' && balance){
-                console.log('SANT!')
-                const receiveBar = document.querySelector('.receive-bar')
-                receiveBar.style.top = '33px'
-                this.#content.innerHTML = `
-                    <h2>${this.#accKey}</h2>
-                    <p>Account value: ${balance}</p>
-                `
+            if(typeof balance === 'number'){
+                this.loggAccount(balance)
+            }else{
+                console.log(balance.message)
             }
         }else{
             this.#resKey = e.target.parentNode.querySelector('.input-key').value
+            const value = parseFloat(document.querySelector('.input-value').value)
+            console.log(value === typeof Number)
             if(this.#accKey === this.#resKey){
-                console.log('You cant send funds to same key...')
+                this.#msg.textContent = 'You cant send funds to same key...'
+            }else if(!isFinite(value)){
+                this.#msg.textContent = 'You have to put in amount ETH. Your not the brightest unit around, are you?'
             }else{
-                const baloon = document.getElementById('popup')
-                if(baloon.style.bottom = '-10px'){
-                    baloon.style.bottom = '200px'
+                // this.liftBaloon()
+                this.#trxHash = await this.wallet.sendTransaction(this.#accKey, this.#resKey, value)
+                if(typeof this.#trxHash === typeof ''){
+                    this.#msg.textContent = 'Of he goes! Hope the network isnt too busy...'
+                    this.liftBaloon()
                 }
-                setTimeout(()=>{
-                    baloon.style.bottom = '-10px'
-                },20000)
             }
         }
     }
 
-    loggAccount(balance){
-        this.#accountDetails.style.top = '35px'
-        this.#transaction.style.top = '33px'
-        console.log(balance)
-        const valueSpan = document.querySelector('#account-balance')
-        valueSpan.textContent = balance
+    liftBaloon(){
+        const baloon = document.getElementById('popup-wrap')
+        const eth = document.querySelector('#eth-div')
+        if(baloon.style.bottom = '-30px'){
+            eth.style.display = "block"
+            baloon.style.bottom = '250px'
+        }
+        setTimeout(()=>{
+            this.#msg.textContent = 'Hey! Hes comming back now, thats got to be a good sign...'
+            eth.style.display = "none";
+            baloon.style.bottom = '-30px'
+        },20000)
+    }
 
+    loggAccount(balance){
+        console.log(this.wallet.status)
+
+        const receiveBar = document.querySelector('.receive-bar')
+        receiveBar.style.top = '40px'
+        this.#content.innerHTML = `
+            <h2>Account value: ${balance}</h2>
+        `
     }
 
     // Tools...
--- scripts/Wallet.js
+++ scripts/Wallet.js
@@ -13,13 +13,14 @@ export default class Wallet{
         console.log(this.status)
     }
 
+    // Gets Metamask public keys
     async getAccounts(){
         if(this.status){
             try {
                 const accounts = await this.#eth.request({
                     method: 'eth_requestAccounts'
                 })
-                console.log(accounts)
+                console.log(typeof accounts)
                 return accounts
             } catch (error) {
                 return false
@@ -27,34 +28,42 @@ export default class Wallet{
         }
     }
 
+    // Checks balance
     async checkBalance(accountKey){
-        const balance = await this.#eth.request({
-            method: 'eth_getBalance',
-            params: [
-                accountKey,
-                'latest'
-            ]
-        })
 
-        const parsedBalance = parseInt(balance) / Math.pow(10, 18)
-
-        return parsedBalance
+        try {
+            const balance = await this.#eth.request({
+                method: 'eth_getBalance',
+                params: [
+                    accountKey,
+                    'latest'
+                ]
+            })
+            const parsedBalance = parseInt(balance) / Math.pow(10, 18)
+            return parsedBalance
+        } catch (error) {
+            return error
+        }
     }
 
     // Sends a transaction...
     async sendTransaction(sender, reciever, amount){
-        const params = [{
-            from: sender,
-            to: reciever,
-            value: amount,
-            gas: Number(21000).toString(16),
-            gasPrice: Number(25000000).toString(16)
-        }]
-
-        const recipe = await this.#eth.request({ 
-            method: "eth_blockNumber", 
-            params: params });
-        
-        return recipe
+        try {
+            const params = [{
+                from: sender,
+                to: reciever,
+                value: (amount * 1e18).toString(16),
+                gas: Number(21000).toString(16),
+                gasPrice: Number(25000000).toString(16)
+            }]
+            const recipe = await this.#eth.request({ 
+                // method: "eth_blockNumber", 
+                method: 'eth_sendTransaction',
+                params: params });
+            
+            return recipe
+        } catch (error) {
+            return error
+        }
     }
 }
